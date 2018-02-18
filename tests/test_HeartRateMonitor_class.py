68f3d3b2a601b484be3aeed4cacdd9586ee192e7
anika-mukherji <anika.mukherji@duke.edu>
2018-02-18 16:25:42 -0500

    Setting up file org & adding preliminary class files + test files

---
+++ tests/test_HeartRateMonitor_class.py
@@ -0,0 +1,6 @@
+def test_HeartRateMonitor_class():
+    try:
+        import pytest
+    except ImportError as e:
+        print("Necessary import failed: {}".format(e))
+    pass
---
+++ tests/test_read_file.py
@@ -0,0 +1,6 @@
+def test_read_file.py
+    try:
+        import pytest
+    except ImportError as e:
+        print("Necessary import failed: {}".format(e))
+    pass
