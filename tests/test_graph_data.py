47d039233b80ddcebe6b546c5a4d6162ac7ca479
anika-mukherji <anika.mukherji@duke.edu>
2018-02-22 09:39:06 -0500

    making tests run faster

--- tests/test_graph_data.py
+++ tests/test_graph_data.py
@@ -6,7 +6,7 @@ def test_graph_data():
     except ImportError as e:
         print("Necessary import failed: {}".format(e))
         return
-    for num in range(1, 33):
+    for num in range(1, 10):
         test = HeartRateMonitor("test_data/test_data{}.csv".format(num))
         res = test.graph_data()
         assert res is True
